b63240c52b7c06a95909c90fb5d8d2bccf1bc8ff
Eduard Musayelyan <eduard@doublecoconut.com>
2023-10-23 12:28:26 +0400

    Implement data transfer to loading service. Refactor.

--- Runtime/BaseLoader.cs
+++ Runtime/BaseLoader.cs
@@ -1,15 +1,32 @@
+using System;
+using System.IO;
 using Cysharp.Threading.Tasks;
 using Zenject;
 
 namespace ContextLoaderService.Runtime
 {
-    public abstract class BaseLoader : ILoadingFlow, IInitializable
+    public abstract class BaseLoader<T> : BaseLoader where T : ILoadData
+    {
+        protected readonly T Data;
+
+        protected BaseLoader(LoadingService loadingService, ILoadData data) : base(loadingService)
+        {
+            if (!(data is T loadData))
+            {
+                throw new InvalidDataException($"Load data is Null or has different type than {typeof(T)}");
+            }
+
+            Data = loadData;
+        }
+    }
+
+    public abstract class BaseLoader : IDisposable, IInitializable
     {
         protected readonly LoadingService LoadingService;
         protected State LoadingStatus { get; private set; }
 
 
-        public BaseLoader(LoadingService loadingService)
+        protected BaseLoader(LoadingService loadingService)
         {
             LoadingService = loadingService;
             LoadingStatus = State.Idle;
@@ -20,13 +37,6 @@ namespace ContextLoaderService.Runtime
             LoadInternal().Forget();
         }
 
-        private async UniTask LoadInternal()
-        {
-            LoadingStatus = State.Loading;
-            await InitWithDelay();
-            LoadingStatus = State.Loaded;
-        }
-
         public async UniTask AwaitCompletion()
         {
             while (LoadingStatus != State.Loaded)
@@ -34,14 +44,22 @@ namespace ContextLoaderService.Runtime
                 await UniTask.DelayFrame(1);
             }
         }
-
-        public abstract UniTask Initialize(ILoadingFlow.IData data); //TODO rework this data transfer.
+        
+        
+        private async UniTask LoadInternal()
+        {
+            LoadingStatus = State.Loading;
+            await InitWithDelay();
+            LoadingStatus = State.Loaded;
+        }
+        
+        protected abstract UniTask Load();
 
         //This should be called because of the scene loading is one frame faster then Loading service's awaiter.
         protected async UniTask InitWithDelay()
         {
             await UniTask.DelayFrame(1);
-            await Initialize(null);
+            await Load();
         }
 
         public virtual void Dispose()
